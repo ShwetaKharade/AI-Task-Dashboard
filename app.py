a95700d7d1887b8da50fb5c5843f48fa7baae606
Shweta Sandip Kharade <106762270+ShwetaKharade@users.noreply.github.com>
2025-05-28 11:56:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,11 @@ import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import joblib
 # Load models and vectorizer
-with open("task_classifier.pkl", "rb") as f:
-    task_classifier = pickle.load(f)
-with open("priority_model.pkl", "rb") as f:
-    priority_model = pickle.load(f)
-with open("tfidf.pkl", "rb") as f:
-    tfidf = pickle.load(f)
+task_classifier = joblib.load("task_classifier.pkl")
+priority_model = joblib.load("priority_model.pkl")
+tfidf_vectorizer = joblib.load("tfidf.pkl")
 
 # App title
 st.title("AI Task Classification and Prioritization")
